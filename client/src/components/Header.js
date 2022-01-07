44411a831f2761c9c2b250f4a8318bc09c924fd7
ksso730 <ksso7300@gmail.com>
2022-01-08 00:30:10 +0900

    Header css animation 추가

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,22 @@
 import React from "react";
-import {Row, Col} from 'reactstrap'
+import {Container, Row, Col} from 'reactstrap'
 
 const Header = () => {
     return (
-        <div id="page-header" className="mb-3 align-me">
+        // <div id="page-header" className="mb-3 align-me">
+        <Container id="page-header">
             <Row>
-                <Col sm="6" sm="auto" className="m-auto">
+                <Col md="6" sm="auto" className="m-auto header_text">
                     <h1>SUYEON KIM</h1>
                     <p>Welcome here, I am back-end developer .</p>
                 </Col>
+                <Col md="6">
+                    <img id="header-back" className="rotate"
+                        src="https://user-images.githubusercontent.com/28856435/148561938-34be59b5-86ca-4699-8d54-21238462500e.png"/>
+                </Col>
             </Row>
-        </div>
+        </Container>
+        // </div>
     )
 }
 
